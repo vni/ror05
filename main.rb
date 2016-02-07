afbd9fb28d7bcfb661fb6dac063d2a53efae9525
Nikolay Vasylchyshyn <younghead@ukr.net>
2016-02-07 10:10:21 +0200

    Done HOMEWORK2:
    * added main.rb

---
+++ main.rb
@@ -0,0 +1,18 @@
+ARGV0 = "main.rb"
+
+# ARGV validation
+if ARGV.length != 4 then
+	puts "Usage: #{ARGV0} x_point y_point x y"
+	exit 1
+end
+x_point, y_point, x, y = ARGV
+
+if x_point == x and y_point == y then
+	puts "Точка найдена!"
+elsif x_point == x then
+	puts "х координата верна, y нет"
+elsif y_point == y then
+	puts "y координата верна, y нет"
+else
+	puts "Близко, но нет"
+end
